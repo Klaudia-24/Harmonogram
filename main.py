a0c6b66cb288b6275493898b4be4252b4dba702e
Klaudia-24 <klaudia.winer@gmail.com>
2021-09-10 17:57:31 +0200

    Changed method for reading data from the JSON file.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from GUI.UI_main_window import *
+from PyQt5 import QtWidgets
+
+from GUI.UI_main_window import Ui_MainWindow
 import sys
+
 
 def __main__():
     app = QtWidgets.QApplication(sys.argv)
