310e7b5e1b5ca8e3d883f8d95c09993d8ce82ae9
Klaudia-24 <klaudia.winer@gmail.com>
2022-03-27 19:05:59 +0200

    Method for time set in the new event (radio button) - part 1.

--- GUI/NewEventW.py
+++ GUI/NewEventW.py
@@ -34,6 +34,7 @@ class NewEventW(QtWidgets.QWidget):
         FileOperationMethods.readFromJsonFileToDict("./events.json", eventsDictionary, "events")
         FileOperationMethods.readFromJsonFileToDict("./eventTypes.json", eventTypesDictionary, "eventTypes")
         self.addEventTypesToComboBox()
+        # self.newEventW.setDurationEventRadioButton.toggled.connect(self.eventDurationDisable)
         self.newEventW.setDurationEventRadioButton.setChecked(True)
 
     def openNewEventTypeWindow(self) -> None:
@@ -135,3 +136,9 @@ class NewEventW(QtWidgets.QWidget):
         for i in tempList:
             for key, value in i.items():
                 self.newEventW.eventTypeComboBox.addItem(key)
+
+    # def eventDurationDisable(self):
+    #
+    #     self.newEventW.allDayEventRadioButton = self.sender()
+    #     if self.newEventW.setDurationEventRadioButton.isChecked():
+    #         pass
