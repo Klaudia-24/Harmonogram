383db5e3bb9bd1d206c3681fe1cb56b9e38640d6
Klaudia-24 <klaudia.winer@gmail.com>
2022-06-18 20:23:37 +0200

    Added calendar view for day.

---
+++ WindowObjects/DayCalendar.py
@@ -0,0 +1,57 @@
+import sys
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import Qt
+
+
+class DayCalendar(QtWidgets.QWidget):
+
+    __startPoint = None
+    __pointsList = []
+    __shapes = []
+
+    def __init__(self, *args, **kwargs):
+        super(DayCalendar, self).__init__(*args, **kwargs)
+        self.resize(2000, 900)
+
+    # def init_ui(self) -> None:
+
+    def mousePressEvent(self, event):
+        self.__startPoint = event.pos()
+        print(event.pos())
+
+    def mouseReleaseEvent(self, event) -> None:
+        self.__shapes.append([self.__startPoint, event.pos()])
+        self.update()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        backgroundBrush = QtGui.QBrush()
+        brush = QtGui.QBrush()
+        backgroundBrush.setColor(QtGui.QColor("#c6ecc6"))
+        backgroundBrush.setStyle(Qt.SolidPattern)
+        painter.fillRect(QtCore.QRect(0, 0, painter.device().width(), painter.device().height()), backgroundBrush)
+
+
+        brush.setColor(QtGui.QColor("#80bfff"))
+        brush.setStyle(Qt.SolidPattern)
+
+        if self.__shapes:
+            for point in self.__shapes:
+                painter.fillRect(QtCore.QRect(point[0].x(), point[0].y(), point[1].x() - point[0].x(), point[1].y() - point[0].y()), brush)
+
+        painter.setFont(QtGui.QFont('Times', self.height()//40, QtGui.QFont.Bold))
+        for i in range(0, 15):
+            # painter.fillRect(QtCore.QRect(10, i*(self.height()//10) + 10, 10, 10), brush)
+            painter.drawText(QtCore.QRect(10, i*(self.height()//10) + 10, self.width()//20, self.height()//30), Qt.AlignVCenter | Qt.AlignHCenter, f"{i}:00")
+
+
+
+def __main__():
+    app = QtWidgets.QApplication(sys.argv)
+    dayCalendar = DayCalendar()
+    dayCalendar.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    __main__()
+
