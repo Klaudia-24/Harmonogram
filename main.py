78878d9eb0b509a57a2c0dbbdf7e5eb9bce0c44f
Klaudia-24 <klaudia.winer@gmail.com>
2022-08-08 17:17:46 +0200

    Added logging settings with a config file.

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from PyQt5 import QtWidgets
 
 from GUI.MainWindow.MainWindow import MainWindow
 import sys
+import logging.config
+
+logging.config.fileConfig('logging.config') # only one import of this file
+logger = logging.getLogger('main')
 
 def __main__():
+    # if we want to run file as standalone
+    # logging.config.fileConfig('logging.config')
     app = QtWidgets.QApplication(sys.argv)
     Mainwindow = MainWindow()
     Mainwindow.show()
