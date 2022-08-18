4f3107d7f8ef9c9d93e26de0226be97723df9894
Klaudia-24 <klaudia.winer@gmail.com>
2022-08-18 22:54:05 +0200

    Added enum for week days with methods.

---
+++ Objects/WeekEvents.py
@@ -0,0 +1,42 @@
+from enum import IntEnum
+import dateutils
+from Objects.Event import getEventsForDay
+
+weekEvents = dict()
+
+class Week(IntEnum):
+
+    Monday = 0
+    Tuesday = 1
+    Wednesday = 2
+    Thursday = 3
+    Friday = 4
+    Saturday = 5
+    Sunday = 6
+
+def clearWeekEventsList():
+    global weekEvents
+    for day in Week:
+        weekEvents[day.name] = []
+
+def getEventsForWeek(firstDayOfWeekDate):
+    global weekEvents
+    clearWeekEventsList()
+
+    for weekDay in Week:
+        dayDate = firstDayOfWeekDate + dateutils.relativedelta(days=(int(weekDay.value)))
+        weekEvents[weekDay.name] = getEventsForDay(dayDate.day, dayDate.month, dayDate.year)
+
+# def getEventsListForWeekDay(weekDay):
+#     global weekEvents
+#     return weekEvents[weekDay]
+
+def __main__():
+    pass
+    # date = datetime.datetime(2022, 8, 8)
+    # l = getEventsForDay(date.day, date.month, date.year)
+    # print(l)
+    # getEventsForWeek(date)
+
+if __name__ == '__main__':
+    __main__()
