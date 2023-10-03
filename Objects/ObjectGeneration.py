b92f8278bcdec350439b29d9af97224fea698623
M1r8n <miros.kowalewski@gmail.com>
2023-10-03 21:50:37 +0200

    Fixed quotation marks in getImageButtonStyleSheet

--- Objects/ObjectGeneration.py
+++ Objects/ObjectGeneration.py
@@ -6,20 +6,23 @@
 # imagePath, minWidth, maxWidth
 # ./WindowObjects/Resources/settingsImage.png
 def getImageButtonStyleSheet(imagePath):
-    return f'QPushButton {{""border-radius: 6px;"\
-           "background-image: url({imagePath});"\
-           "background-position:center;" \
-           "background-repeat:no-repeat;" \
-           "min-width: 60px;" \
-           "max-width: 50px;" \
-           "}}"\
-           "QPushButton:pressed {{"\
-           "background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,"\
-           "stop: 0 #dadbde, stop: 1 #f6f7fa);"\
-           "}}"\
-           "QPushButton:flat {{"\
-           "border: none; /* no border for a flat push button */"\
-           "}}"\
-           "QPushButton:default {{"\
-           "border-color: navy; /* make the default button prominent */"\
-           "}}'
+    return f'QPushButton {{border-radius: 6px;\
+            background-image: url({imagePath});\
+           background-position:center; \
+           background-repeat:no-repeat; \
+           min-width: 60px; \
+           max-width: 50px; \
+           }}\
+           QPushButton:pressed {{\
+           background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,\
+           stop: 0 #dadbde, stop: 1 #f6f7fa);\
+           }}\
+           QPushButton:flat {{\
+           border: none;\
+           }}\
+           QPushButton:default {{\
+           border-color: navy; \
+           }}'
+
+if __name__=="__main__":
+    print(getImageButtonStyleSheet("./WindowObjects/Resources/settingsImage.png"))
